1606096f40b09cac417e26df3fc9329bc02f69d8
jeffrey2545 <jeffrey2545@gmail.com>
2022-08-14 01:42:26 +0800

    Time: 129 ms (5.31%), Space: 43.1 MB (25.18%) - LeetHub

---
+++ 809-expressive-words/809-expressive-words.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int expressiveWords(String s, String[] words) {
+        int result = 0;
+        
+        for (int i = 0; i < words.length; i++) {
+            result = result + isStretchy(s, words[i]);
+        }
+        
+        return result;
+    }
+    
+    public int isStretchy(String stretchy, String regular) {
+        int regPoint = 0;
+        char[] regChar = regular.toCharArray();
+        char regtmp = regChar[0];
+        int regCount = 0;
+        
+        int strPoint = 0;
+        char[] strChar = stretchy.toCharArray();
+        char strtmp = strChar[0];
+        int strCount = 0;
+        System.out.println("regular.length():" + regular.length() + " stretchy.length():" + stretchy.length());
+        while (strPoint < stretchy.length() && regPoint < regular.length()) {
+            regtmp = regChar[regPoint];
+            regCount = 0;
+            strtmp = strChar[strPoint];
+            strCount = 0;
+            while (regPoint < regular.length() && regChar[regPoint] == regtmp) {
+                regPoint++;
+                regCount++;
+            }
+            System.out.println("regtmp:" + regtmp + " regPoint:" + regPoint + " regCount:" + regCount);
+            while (strPoint < stretchy.length() && strChar[strPoint] == strtmp) {
+                strPoint++;
+                strCount++;
+            }
+            System.out.println("strtmp:" + strtmp + " strPoint:" + strPoint + " strCount:" + strCount);
+
+            if (regtmp != strtmp) {
+                return 0;
+            }
+            if (strCount >= regCount && strCount != 2) {
+                continue;
+            } else if (strCount == 2 && regCount == 2) {
+                continue;
+            } else {
+                return 0;
+            }
+        }
+        
+        if (strPoint < stretchy.length() || regPoint < regular.length()) {
+            return 0;
+        }
+        
+        return 1;
+    }
+}
