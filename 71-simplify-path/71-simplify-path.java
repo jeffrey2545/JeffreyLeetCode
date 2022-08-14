78fef964c68ad345549c03750dba073b0d253c7e
jeffrey2545 <jeffrey2545@gmail.com>
2022-08-14 18:16:06 +0800

    Time: 15 ms (23.79%), Space: 42.7 MB (88.11%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] pathArray = path.split("/+");
+        Stack<String> stack = new Stack<>();
+
+        for (String s : pathArray) {
+            if ("..".equals(s)) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if ("".equals(s) || ".".equals(s)) {
+                continue;
+            } else {
+                stack.push(s);
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return "/";
+        }
+        String result = "";
+        while (!stack.isEmpty()) {
+            result = "/" + stack.pop() + result;
+        }
+        return result;
+    }
+}
