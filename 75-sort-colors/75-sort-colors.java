af6e83fcc1a046dbacd2f95a7db2e64c528c7e66
jeffrey2545 <jeffrey2545@gmail.com>
2022-09-05 00:38:22 +0800

    Time: 1 ms (42.15%), Space: 42.1 MB (76.94%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,5 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int[] tmp = new int[nums.length];
+        split(nums, 0, nums.length - 1, tmp);
+    }
+    public void split(int[] arr, int start, int end, int[] tmp) {
+        if (start >= end) {
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        split(arr, start, mid, tmp);
+        split(arr, mid + 1, end, tmp);
+        merge(arr, start, mid, end, tmp);
+    }
+    public void merge(int[] arr, int start, int mid, int end, int[] tmp) {
+        int left = start;
+        int right = mid + 1;
+        for (int i = 0; i < end - start + 1; i++) {
+            if (left <= mid && (right > end || (arr[left] < arr[right]))) {
+                tmp[i] = arr[left++];
+            } else {
+                tmp[i] = arr[right++];
+            }
+        }
+        for (int k = 0; k < end - start + 1; k++) {
+            arr[start + k] = tmp[k];
+        }
     }
 }
