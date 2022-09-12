4c2f34e1f52245204c908eada231a03375281265
jeffrey2545 <jeffrey2545@gmail.com>
2022-09-12 11:45:58 +0800

    Time: 6 ms (5.03%), Space: 42.4 MB (75.26%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length < 1) {
+            return false;
+        }
+        
+        int startPoint = 0;
+        int endPoint = (matrix[0].length * matrix.length) - 1;
+        while (startPoint + 1 < endPoint) {
+            int midPoint = (startPoint + endPoint) / 2;
+            int mid = oneToTwo(matrix, midPoint);
+            System.out.println(mid);
+            if (mid == target) {
+                return true;
+            }
+            if (mid > target) {
+                endPoint = midPoint;
+            }
+            if (mid < target) {
+                startPoint = midPoint;
+            }
+        }
+        
+        int start = oneToTwo(matrix, startPoint);
+        int end = oneToTwo(matrix, endPoint);
+        if (target == start ||target == end) {
+            return true;
+        }
+        return false;
+    }
+    
+    public int oneToTwo(int[][] matrix, int pointer) {
+        // if (pointer <= matrix) {}
+        int x = pointer / matrix[0].length;
+        int y = (pointer % matrix[0].length);
+        // System.out.println(x);
+        // System.out.println(y);
+        return matrix[x][y];
+    }
+}
