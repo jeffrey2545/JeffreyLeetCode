b212172df691dd01b021d07ac699d1d9ff4036ec
jeffrey2545 <jeffrey2545@gmail.com>
2022-12-30 01:38:21 -0500

    Time: 127 ms (5.00%), Space: 38.9 MB (93.09%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,25 @@
 class Solution {
+    int[] memo = new int[32];
     public int fib(int n) {
+        Arrays.fill(memo, -1);
+        return dfs(n);
+    }
+    
+    public int dfs(int n) {
         if (n == 0) {
             return 0;
         }
         if (n == 1) {
             return 1;
         }
-        return fib(n - 1) + fib(n - 2);
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+        
+        int f = fib(n - 1) + fib(n - 2);
+        // System.out.println(n);
+        // System.out.println(memo.length);
+        memo[n] = f;
+        return f;
     }
 }
