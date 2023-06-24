fbca657e514170b62c99e3a65e36c0ee1ffb4985
Jeffrey Chen <jeffrey2545@gmail.com>
2023-06-24 15:20:42 -0500

    Time: 10 ms (5.01%), Space: 44.8 MB (5.20%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,44 +1,29 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (candidates.length == 0) {
-            return result;
-        }
-        // int su = Arrays.stream(candidates).sum();
-        // int len = candidates.length;
-        Set<List<Integer>> set = new HashSet<>();
-        List<Integer> path = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(candidates);
-        dfs(candidates, path, set, target, 0);
-        
-        for (List<Integer> list : set) {
-            result.add(list);
-        }
-        return result;
+        dfs(ans, new ArrayList<>(), target, candidates, 0);
+        return ans;
     }
     
-    public void dfs(int[] candidates, List<Integer> path, Set<List<Integer>> set, int target, int index) {
-        if (candidates.length == index) {
+    public void dfs(List<List<Integer>> ans, List<Integer> combination, int target, int[] candidates, int index) {
+        if (target < 0) {
             return;
         }
-
-        for (int i = index; i < candidates.length; i++) {
-            if(i > index && candidates[i] == candidates[i-1]) continue; /** skip duplicates */
-            int candidate = candidates[i];
-            if (target - candidate == 0) {
-                path.add(candidate);
-                set.add(new ArrayList<>(path));
-                path.remove(path.size() - 1);
-                break;
-            } else if (target - candidate < 0) {
-                break;
-            } else {
-                path.add(candidate);
-                dfs(candidates, path, set, target - candidate, i + 1);
-                path.remove(path.size() - 1);
-            }
+        if (target == 0) {
+            ans.add(new ArrayList<>(combination));
+            return;
         }
         
-        return;
+        HashSet<Integer> visited = new HashSet<>();
+        for (int i = index; i < candidates.length; i++) {
+            if (visited.contains(candidates[i])) {
+                continue;
+            }
+            visited.add(candidates[i]);
+            combination.add(candidates[i]);
+            dfs(ans, combination, target - candidates[i], candidates, i + 1);
+            combination.remove(combination.size() - 1);
+        }
     }
 }
