5445b429ba4fb4a507dba8cc4e181b617e8a635a
Jeffrey Chen <jeffrey2545@gmail.com>
2023-07-13 06:00:10 -0500

    Time: 0 ms (100.00%), Space: 40.7 MB (90.55%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,68 +1,34 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums.length == 0 || nums == null) {
-            return -1;
-        }
-        if (nums.length == 1) {
-            if (nums[0] == target) {
-                return 0;
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[nums.length - 1]) {
+                left = mid + 1;
+            } else if (nums[mid] < nums[nums.length - 1]) {
+                right = mid - 1;
             } else {
-                return -1;
+                right = mid - 1;
             }
         }
         
-        int start = 0;
-        int end = nums.length - 1;
-        int minIndex = 0;
-        while (start + 1 < end) {
-            int mid = (start + end) / 2;
-            if (nums[mid] > nums[end]) {
-                start = mid;
-            } else {
-                end = mid;
-            }
+        int leftSide = bs(nums, target, 0, left - 1);
+        if (leftSide != -1) {
+            return leftSide;
         }
-        if (nums[start] > nums[end]) {
-            minIndex = end;
-        } else {
-            minIndex = start;
-        }
-        int maxIndex = 0;
-        if (minIndex == 0) {
-            maxIndex = nums.length - 1;
-        } else {
-            maxIndex = minIndex - 1;
-        }
-        if (target > nums[maxIndex] || target < nums[minIndex]) {
-            return -1;
-        }
-        
-        // System.out.println(minIndex);
-        
-        if (target == nums[minIndex]) {
-            return minIndex;
-        }
-        if (target > nums[nums.length - 1]) {
-            start = 0;
-            end = minIndex - 1;
-        } else {
-            start = minIndex;
-            end = nums.length - 1;
-        }
-        while (start + 1 < end) {
-            int mid = (start + end) / 2;
+        return bs(nums, target, left, nums.length - 1);
+    }
+    private int bs(int[] nums, int target, int left, int right) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             if (nums[mid] > target) {
-                end = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             } else {
-                start = mid;
+                return mid;
             }
         }
-        if (nums[start] == target) {
-            return start;
-        } else if (nums[end] == target) {
-            return end;
-        } else {
-            return -1;
-        }
+        return -1;
     }
 }
