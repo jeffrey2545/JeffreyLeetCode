bba6ea96784117fd9ea48cc41eeaafa419c0fa6d
Jeffrey Chen <jeffrey2545@gmail.com>
2023-07-13 00:27:27 -0500

    Time: 0 ms (100.00%), Space: 44.6 MB (78.05%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,24 +1,16 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums.length == 0) {
-            return -1;
-        }
-        
-        int left = 0;
-        int right = nums.length - 1;
-        
+        int left = 0, right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            
-            if (target > nums[mid]) {
-                left = mid + 1;
-            } else if (target < nums[mid]) {
+            if (nums[mid] > target) {
                 right = mid - 1;
-            } else if (target == nums[mid]) {
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
                 return mid;
             }
         }
-        
         return -1;
     }
 }
