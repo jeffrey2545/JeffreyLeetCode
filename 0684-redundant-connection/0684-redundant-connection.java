2cc9bfbca83e0952cd483fd1bbb525e4ac0e5e0a
Jeffrey Chen <jeffrey2545@gmail.com>
2023-08-07 05:20:52 -0500

    Time: 1 ms (76.19%), Space: 43.7 MB (39.92%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -0,0 +1,54 @@
+class Solution {
+    class UF {
+        int[] parent;
+        int[] rank;
+        
+        public UF(int size) {
+            parent = new int[size];
+            rank = new int[size];
+            for (int i = 0; i < size; i++) {
+                parent[i] = i;
+            }
+        }
+        
+        public int find(int a) {
+            if (parent[a] == a) {
+                return a;
+            }
+            parent[a] = find(parent[a]);
+            return parent[a];
+        }
+        
+        public boolean union(int a, int b) {
+            int aParent = find(a);
+            int bParent = find(b);
+            
+            if (aParent == bParent) {
+                return false;
+            }
+            
+            if (rank[aParent] > rank[bParent]) {
+                parent[bParent] = aParent;
+            } else if (rank[aParent] < rank[bParent]) {
+                parent[aParent] = bParent;
+            } else {
+                parent[aParent] = bParent;
+                rank[bParent]++;
+            }
+            
+            return true;
+        }
+    }
+    
+    public int[] findRedundantConnection(int[][] edges) {
+        UF uf = new UF(1001);
+        for (int[] edge : edges) {
+            int a = edge[0];
+            int b = edge[1];
+            if (!uf.union(a, b)) {
+                return edge;
+            }
+        }
+        return edges[0];
+    }
+}
