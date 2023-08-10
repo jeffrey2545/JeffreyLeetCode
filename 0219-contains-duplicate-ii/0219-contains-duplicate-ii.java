90cdae0cddb88b66149aa4c377bec1571eb22be5
Jeffrey Chen <jeffrey2545@gmail.com>
2023-08-10 10:59:48 -0500

    Time: 18 ms (81.71%), Space: 55 MB (87.38%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
