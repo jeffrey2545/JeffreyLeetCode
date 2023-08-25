2e0a70a7b6a384d375b5cb6cf719509698ce8ca8
Jeffrey Chen <jeffrey2545@gmail.com>
2023-08-25 00:17:46 -0500

    Time: 0 ms (100.00%), Space: 50.4 MB (16.46%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,28 +1,28 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
-        int left = 0, right = nums.length - 1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
             if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
                 return nums[mid];
             }
-            if (mid % 2 == 1) {
-                if (nums[mid] == nums[mid - 1]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            } else {
-                if (nums[mid] == nums[mid - 1]) {
-                    right = mid;
-                } else {
-                    left = mid + 1;
-                }
+            if ((nums[mid] == nums[mid - 1] && mid % 2 == 1) || (nums[mid] == nums[mid + 1] && mid % 2 == 0))  { // right
+                start = mid;
+            } else { // left
+                end = mid;
             }
         }
-        return nums[left];
+        
+        if (start == 0) {
+            return nums[start];
+        }
+        if (end == nums.length - 1) {
+            return nums[end];
+        }
+        if (nums[start] == nums[start + 1] || nums[start] == nums[start - 1]) {
+            return nums[end];
+        } else {
+            return nums[start];
+        }
     }
 }
