9fac4ee67cabb2d36e4e1459e2af1b94a828186c
Jeffrey Chen <jeffrey2545@gmail.com>
2023-08-27 10:28:21 -0500

    Time: 26 ms (61.54%), Space: 46.7 MB (27.85%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,21 +1,28 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        Map<Integer, Integer> prefixMap = new HashMap<>();
-        prefixMap.put(0, 1);
-        int prefix = 0;
-        int result = 0;
-        for (int i = 0; i < nums.length; i++) {
-            prefix = prefix + nums[i];
-            if (prefixMap.containsKey(prefix - k)) {
-                result = result + prefixMap.get(prefix - k);
-            }
-            if (prefixMap.containsKey(prefix)) {
-                int origin = prefixMap.get(prefix);
-                prefixMap.put(prefix, origin + 1);
-            } else {
-                prefixMap.put(prefix, 1);
-            }
+        int count = 0, currSum = 0;
+        HashMap<Integer, Integer> h = new HashMap();
+        
+        for (int num : nums) {
+            // current prefix sum
+            currSum += num;
+            
+            // situation 1:  
+            // continuous subarray starts 
+            // from the beginning of the array
+            if (currSum == k)
+                count++;
+            
+            // situation 2:
+            // number of times the curr_sum − k has occured already, 
+            // determines the number of times a subarray with sum k 
+            // has occured upto the current index
+            count += h.getOrDefault(currSum - k, 0);
+            
+            // add the current sum
+            h.put(currSum, h.getOrDefault(currSum, 0) + 1);    
         }
-        return result;
+                
+        return count;
     }
 }
