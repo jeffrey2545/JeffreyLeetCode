78b2fea03684332701c0ac59ddcefc2515d355f5
Jeffrey Chen <jeffrey2545@gmail.com>
2023-08-28 22:36:19 -0500

    Time: 1 ms (78.92%), Space: 43.8 MB (29.04%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,47 +1,48 @@
-class Solution {
-    class UF {
-        int[] parent;
-        int[] rank;
-        
-        public UF(int size) {
-            parent = new int[size];
-            rank = new int[size];
-            for (int i = 0; i < size; i++) {
-                parent[i] = i;
-            }
-        }
-        
-        public int find(int a) {
-            if (parent[a] == a) {
-                return a;
-            }
-            parent[a] = find(parent[a]);
-            return parent[a];
-        }
-        
-        public boolean union(int a, int b) {
-            int aParent = find(a);
-            int bParent = find(b);
-            
-            if (aParent == bParent) {
-                return false;
-            }
-            
-            if (rank[aParent] > rank[bParent]) {
-                parent[bParent] = aParent;
-            } else if (rank[aParent] < rank[bParent]) {
-                parent[aParent] = bParent;
-            } else {
-                parent[aParent] = bParent;
-                rank[bParent]++;
-            }
-            
-            return true;
+class UF {
+    int[] parent;
+    int[] rank;
+    
+    public UF(int size) {
+        parent = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < size; i++) {
+            parent[i] = i;
         }
     }
     
+    public int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        parent[a] = find(parent[a]);
+        return parent[a];
+    }
+    
+    public boolean union(int a, int b) {
+        int aParent = find(a);
+        int bParent = find(b);
+        
+        if (aParent == bParent) {
+            return false;
+        }
+        
+        if (rank[aParent] > rank[bParent]) {
+            parent[bParent] = aParent;
+        } else if (rank[aParent] < rank[bParent]) {
+            parent[aParent] = bParent;
+        } else {
+            parent[aParent] = bParent;
+            rank[bParent]++;
+        }
+        
+        return true;
+    }
+}
+
+class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-        UF uf = new UF(1001);
+        int n = edges.length;
+        UF uf = new UF(n + 1);
         for (int[] edge : edges) {
             int a = edge[0];
             int b = edge[1];
