0bdfb81893d5d776fceff9128db73674ea38ebdb
Jeffrey Chen <jeffrey2545@gmail.com>
2023-09-10 21:47:22 -0500

    Time: 6 ms (81.69%), Space: 44.7 MB (28.14%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,32 +1,24 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        // sorted by start time
-        Arrays.sort(intervals, (int[] intervalA, int[] intervalB) -> intervalA[0] - intervalB[0]);
-        // pick interval from intervals
-        // put it in list
-        List<int[]> rooms =  new ArrayList<>();
-        rooms.add(intervals[0]);
-        int ans = 1;
-        // every time one new interval into list, check if there is place
-        // every time the list size changed, update the max rooms
-        for (int i = 1; i < intervals.length; i++) {
-            int[] interval = intervals[i];
-            boolean change = false;
-            for (int r = 0; r < rooms.size(); r++) {
-                int[] room = rooms.get(r);
-                if (interval[0] >= room[1]) {
-                    rooms.set(r, interval);
-                    change = true;
-                    break;
-                }
-            }
-            
-            if (!change) {
-                rooms.add(interval);
-            }
-            ans = Math.max(ans, rooms.size());
+        PriorityQueue<int[]> mainHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        for (int[] interval : intervals) {
+            mainHeap.add(interval);
         }
         
+        int[] first = mainHeap.poll();
+        PriorityQueue<Integer> smallHeap = new PriorityQueue<>();
+        smallHeap.add(first[1]);
+        int ans = smallHeap.size();
+        while (!mainHeap.isEmpty()) {
+            int[] interval = mainHeap.poll();
+            if (interval[0] < smallHeap.peek()) {
+                smallHeap.add(interval[1]);
+            } else {
+                smallHeap.poll();
+                smallHeap.add(interval[1]);
+            }
+            ans = Math.max(ans, smallHeap.size());
+        }
         return ans;
     }
 }
