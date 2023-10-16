b0574c758453fd673eab1b00380957346c0c8048
Jeffrey Chen <jeffrey2545@gmail.com>
2023-10-16 17:02:18 -0500

    Time: 0 ms (100.00%), Space: 43.9 MB (42.59%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,70 +13,22 @@
  *     }
  * }
  */
-class Node {
-    boolean isBST;
-    int maxNode;
-    int minNode;
-    Node() {
-        
-    }
-    Node(boolean isBST, int maxNode, int minNode) {
-        this.isBST = isBST;
-        this.maxNode = maxNode;
-        this.minNode = minNode;
-    }
-}
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return dfs(root).isBST;
+        return helper(root, null, null);
     }
     
-    public Node dfs(TreeNode root) {
-        if (root.left == null && root.right == null) {
-            Node child = new Node(true, root.val, root.val);
-            return child;
+    public boolean helper(TreeNode root, Integer max, Integer min) {
+        if (root == null) {
+            return true;
         }
         
-        Node left = new Node();
-        if (root.left != null) {
-            left = dfs(root.left);
+        if (min != null && min >= root.val) {
+            return false;
         }
-        Node right = new Node();
-        if (root.right != null) {
-            right = dfs(root.right);
+        if (max != null && max <= root.val) {
+            return false;
         }
-        
-        Node result = new Node(false, 0, 0);
-        if (left.isBST && right.isBST) {
-            if (root.val > left.maxNode && root.val < right.minNode) {
-                result.isBST = true;
-                result.maxNode = right.maxNode;
-                result.minNode = left.minNode;
-                return result;
-            } else {
-                return result;
-            }
-        } else if (left.isBST && root.right == null) {
-            if (root.val > left.maxNode) {
-                result.isBST = true;
-                result.maxNode = root.val;
-                result.minNode = left.minNode;
-                return result;
-            } else {
-                return result;
-            }
-        } else if (right.isBST && root.left == null) {
-            if (root.val < right.minNode) {
-                result.isBST = true;
-                result.maxNode = right.maxNode;
-                result.minNode = root.val;
-                return result;
-            } else {
-                return result;
-            }
-        }
-        else {
-            return result;
-        }
+        return helper(root.left, root.val, min) && helper(root.right, max, root.val);
     }
 }
