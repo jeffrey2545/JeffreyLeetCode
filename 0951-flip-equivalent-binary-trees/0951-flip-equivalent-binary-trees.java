1fcd9ca930ecab0ff9548abbe866b9a189a2a43a
Jeffrey Chen <jeffrey2545@gmail.com>
2023-11-05 21:41:08 -0500

    Time: 0 ms (100.00%), Space: 39.9 MB (88.27%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
@@ -15,12 +15,20 @@
  */
 class Solution {
     public boolean flipEquiv(TreeNode root1, TreeNode root2) {
-        if (root1 == root2)
+        if (root1 == null && root2 == null) {
             return true;
-        if (root1 == null || root2 == null || root1.val != root2.val)
+        }
+        if (root1 == null) {
             return false;
-
-        return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right) ||
-                flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+        }
+        if (root2 == null) {
+            return false;
+        }
+        
+        return root1.val == root2.val && ((
+            flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)
+        ) || (
+            flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left)
+        ));
     }
 }
