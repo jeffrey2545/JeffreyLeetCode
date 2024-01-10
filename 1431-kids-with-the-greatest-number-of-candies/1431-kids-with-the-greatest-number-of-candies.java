60830ad5536100cb97390e99316970207cd727f9
Jeffrey Chen <jeffrey2545@gmail.com>
2024-01-11 02:20:31 +0800

    Time: 1 ms (93.34%), Space: 41.8 MB (59.59%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = Integer.MIN_VALUE;
+        for (int candy : candies) {
+            if (candy >= max) {
+                max = candy;
+            }
+        }
+        
+        List<Boolean> ans = new ArrayList<>();
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= max) {
+                ans.add(true);
+            } else {
+                ans.add(false);
+            }
+        }
+        
+        return ans;
+    }
+}
