da0eacf1bfc28210a0deb6ceb1c71ecb08d3b7af
Jeffrey Chen <jeffrey2545@gmail.com>
2024-01-23 20:27:13 -0500

    Time: 34 ms (35.98%), Space: 59.8 MB (5.38%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
