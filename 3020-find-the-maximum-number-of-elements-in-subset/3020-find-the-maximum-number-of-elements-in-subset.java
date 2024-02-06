c6ec2daf7dd5625c1d021e14efc6b8aa94dc5bf9
Jeffrey Chen <jeffrey2545@gmail.com>
2024-02-06 10:40:15 -0500

    Time: 174 ms (15.98%), Space: 56 MB (73.87%) - LeetHub

---
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maximumLength(int[] nums) {
+        int maxCount = 1;
+        // build map
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        if (map.containsKey(1)) {
+            int one = map.get(1);
+            if (one % 2 == 0) {
+                one = one - 1;
+            }
+            maxCount = Math.max(maxCount, one);
+            map.remove(1);
+        }
+        // iterate the key of map
+        for (Integer key : map.keySet()) {
+            // if (map.get(key) < 2) {
+            //     continue;
+            // }
+            int currCount = 1;
+            int x = key;
+            while (map.containsKey(x * x) && map.get(x) >= 2) {
+                currCount = currCount + 2;
+                x = x * x;
+            }
+            maxCount = Math.max(maxCount, currCount);
+        }
+        
+        return maxCount;
+    }
+}
